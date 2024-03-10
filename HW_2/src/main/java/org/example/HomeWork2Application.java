447db27dec4e5299ed43f039d4ef2a98aa9aab30
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-03-10 18:30:42 +0300

    Добавили решение дз-5

---
+++ HW_2/src/main/java/org/example/HomeWork2Application.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Главный класс приложения.
+ */
+@SpringBootApplication
+public class HomeWork2Application {
+
+    /**
+     * Метод для запуска приложения.
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork2Application.class, args);
+    }
+
+}
