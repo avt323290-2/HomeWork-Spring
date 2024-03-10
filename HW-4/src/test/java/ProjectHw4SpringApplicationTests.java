133842c099254babe27e082f8d523bc448844ff9
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-03-10 16:57:07 +0300

    Добавили решение дз №4

---
+++ HW-4/src/test/java/ProjectHw4SpringApplicationTests.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class ProjectHw4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
