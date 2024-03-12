e908f2da0d2cb68b6472e93f2885f0e1d8c7081d
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-03-12 15:42:51 +0300

    Добавили решение дз №6

---
+++ HW6/src/main/java/ru/geekbrains/HW6/repository/NoteRepo.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.HW6.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.geekbrains.HW6.model.Note;
+
+import java.util.Optional;
+
+@Repository
+public interface NoteRepo extends JpaRepository<Note, Long> {
+
+    /**
+     * Найти заметку по id
+     * @param id must not be {@literal null}.
+     * @return возвращает заметку.
+     */
+    Optional<Note> findById(Long id);
+}
