679d3ad61a34cdaa2e0ebf224efc7d2b472c2b2b
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-03-12 15:42:51 +0300

    Добавили условие дз №6

---
+++ HW6/src/test/java/ru/geekbrains/HW6/Hw6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.HW6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
