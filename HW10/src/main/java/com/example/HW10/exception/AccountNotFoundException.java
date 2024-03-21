cffda34e0dbf51b58a23cac9db5eb0d44110b128
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-03-21 19:45:16 +0300

    Добавили решение дз 10

---
+++ HW10/src/main/java/com/example/HW10/exception/AccountNotFoundException.java
@@ -0,0 +1,15 @@
+package com.example.HW10.exception;
+
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException() {
+        super();
+    }
+
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+
+    public AccountNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
