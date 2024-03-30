1884cc8453216c959eb68e40850bf068aef63954
Smirnov Mihail <smirnov_mikhael@bk.ru>
2024-03-30 20:09:01 +0300

    Добавили несколько тестов в HW12

--- HW12/src/test/java/com/example/HW12/Hw12ApplicationTests.java
+++ HW12/src/test/java/com/example/HW12/Hw12ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.HW12;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class Hw12ApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Hw12ApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	public void testHealthEndpoint() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/actuator/health", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("UP");
+	}
+
+	@Test
+	public void testMainPageAccess() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).contains("Welcome to the Main Page");
 	}
 
 }
